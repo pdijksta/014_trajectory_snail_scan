fbde3ea1fed1054bd87a19f418d1d51afbb0ab6f
philipp <philipp.dijkstal@desy.de>
2023-05-13 13:15:50 +0200

    minor

--- config.py
+++ config.py
@@ -56,4 +56,4 @@ bpm_channels = {
         'SASE3': 'BPM',
         }
 
-energy_ch = lambda beamline: 'XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/T4/ENERGY.SA%i' % int(beamline[-1])
+energy_ch = lambda beamline: 'XFEL.DIAG/BEAM_ENERGY_MEASUREMENT/CL/ENERGY.SA%i' % int(beamline[-1])
