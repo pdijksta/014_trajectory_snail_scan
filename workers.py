b0ab76faaa6bd112b1456e791949188863c0a675
dijkstal <philipp.dijkstal@desy.de>
2023-05-13 16:57:38 +0200

    cleanup

--- workers.py
+++ workers.py
@@ -1,15 +1,5 @@
 from PyQt5.QtCore import QObject, pyqtSignal
 
-#class Thread(QThread):
-#    def __init__(self, parent):
-#        QThread.__init__(self, parent=parent)
-#        self.abort = False
-#
-#    def stop(self):
-#        self.abort = True
-#        #self.wait()
-
-
 class WorkerBase(QObject):
     """
     Wraps a function call such that it can be used by QThread.
@@ -29,11 +19,9 @@ class WorkerBase(QObject):
     def run(self):
         try:
             self.outp = self.func(*self.args, **self.kwargs)
-            print('Run status')
         except Exception as e:
             self.error = True
-            print('%s failed with following error message:\n%s' % (self.function, e))
+            print('Worker failed with following error message:\n%s' % e)
         finally:
-            print('finished.emit')
             self.finished.emit()
 
